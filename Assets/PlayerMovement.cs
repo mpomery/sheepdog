eb91634a3b5d688f50b630d94fd1b82274a042b7
Mitchell Pomery <bob_george33@hotmail.com>
2019-10-19 18:00:00 +1100

    Camera Controls and Movement for 1 player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Transform playerCamera;
+
+    // Camera angle and rotation is relative to games axis, not player
+    private float cameraElevationAngle = 10; // 10 to 45 degrees
+    private float cameraRotation = 180; // 0 - 360 degrees
+    private readonly float cameraDistance = 40; // How far away from the player the camera should sit
+
+    private float playerAngle = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player Initialized");
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the camera
+        var verticalCameraInput = Input.GetAxis("Camera X");
+        var horizontalCameraInput = Input.GetAxis("Camera Y");
+
+        cameraRotation = (cameraRotation - verticalCameraInput) % 360;
+        cameraElevationAngle += horizontalCameraInput;
+        if (cameraElevationAngle > 45)
+        {
+            cameraElevationAngle = 45;
+        }
+        else if (cameraElevationAngle < 10)
+        {
+            cameraElevationAngle = 10;
+        }
+
+        //Debug.Log($"cameraRotation: {cameraRotation}"); // Rotate around the Y Axis
+        //Debug.Log($"cameraElevationAngle: {cameraElevationAngle}");
+
+        // Place the camera relative to the player
+        var cameraOffset = new Vector3(0, 0, cameraDistance);
+        cameraOffset = Quaternion.Euler(-cameraElevationAngle, cameraRotation, 0f) * cameraOffset;
+        playerCamera.position = transform.position + cameraOffset;
+        playerCamera.rotation = Quaternion.Euler(cameraElevationAngle, cameraRotation + 180, 0f);
+
+
+        // Calculate input amount
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var inputSpeed = Mathf.Sqrt(Mathf.Pow(verticalInput, 2) + Mathf.Pow(horizontalInput, 2));
+        // Cap input speed to 1 to prevent keyboard from going faster
+        if (inputSpeed > 1)
+        {
+            inputSpeed = 1;
+        }
+        
+        // Input Threshold
+        if (inputSpeed > 0.3)
+        {
+            // Calucate the movement direction input angle
+            var theta = Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg;
+            playerAngle = theta + cameraRotation;
+            //Debug.Log($"theta: {theta}");
+            //Debug.Log($"playerAngle: {playerAngle}");
+
+            // Look Towards that angle
+            var step = 1000 * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(90, playerAngle, 0), step);
+
+            rb.velocity = Quaternion.Euler(0, playerAngle + 90, 0) * new Vector3(inputSpeed * 10, 0, 0);
+        }
+
+    }
+
+    // FixedUpdate is called once per physics frame
+    void FixedUpdate()
+    {
+
+        //rb.AddForce(new Vector3(horizontalInput, 0.0f, verticalInput) * moveSpeed * Time.deltaTime);
+
+        //rb.velocity = new Vector3(verticalInput * moveSpeed * Time.deltaTime, 0, horizontalInput * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/SheepAI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAI : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameObject dog;
+    private float angle;
+    private static int startleDistance = 15;
+    private static int moveSpeed = 400;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dog = GameObject.Find("Dog");
+        Debug.Log("Player Initialized");
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(dog.transform.position);
+
+        var heading = transform.position - dog.transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+        var angle = Vector3.Angle(dog.transform.position, transform.position);
+        Debug.Log($"heading: {heading}");
+        Debug.Log($"distance: {distance}");
+        Debug.Log($"direction: {direction}");
+
+        var theta = Mathf.Atan2(heading.x, heading.z) * Mathf.Rad2Deg;
+        angle = theta + 180;
+
+        Debug.Log($"angle: {angle}");
+
+        if (distance < startleDistance)
+        {
+            var step = 1000 * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(90, angle, 0), step);
+
+            var speed = (startleDistance - distance) / startleDistance;
+
+            rb.velocity = Quaternion.Euler(0, angle + 90, 0) * new Vector3(speed * 10, 0, 0);
+        }
+    }
+}
