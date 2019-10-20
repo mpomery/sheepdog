38240825f7614c681e0c8f7cb640420a0cce1009
Mitchell Pomery <bob_george33@hotmail.com>
2019-10-20 22:02:57 +1100

    Simple Goal that checks all sheep are in it

---
+++ Assets/Goal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private static int goalRadius = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(2 * goalRadius, 1, 2 * goalRadius);
+        var sheep = FindObjectsOfType(typeof(SheepAI)) as SheepAI[];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var closeSheep = 0;
+
+        var sheep = FindObjectsOfType(typeof(SheepAI)) as SheepAI[];
+        //Debug.Log(sheep.Length);
+
+        foreach (var s in sheep)
+        {
+            var distance = (transform.position - s.transform.position).magnitude;
+            if (distance < goalRadius)
+            {
+                closeSheep++;
+            }
+        }
+
+        Debug.Log($"{closeSheep}/{sheep.Length}");
+
+        if (closeSheep == sheep.Length)
+        {
+            Debug.Log($"All Sheep Herded");
+        }
+    }
+}
--- Assets/SheepAI.cs
+++ Assets/SheepAI.cs
@@ -7,14 +7,13 @@ public class SheepAI : MonoBehaviour
     private Rigidbody rb;
     private GameObject dog;
     private float angle;
-    private static int startleDistance = 15;
+    private static int startleDistance = 10;
     private static int moveSpeed = 400;
 
     // Start is called before the first frame update
     void Start()
     {
         dog = GameObject.Find("Dog");
-        Debug.Log("Player Initialized");
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
@@ -22,20 +21,20 @@ public class SheepAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(dog.transform.position);
+        //Debug.Log(dog.transform.position);
 
         var heading = transform.position - dog.transform.position;
         var distance = heading.magnitude;
         var direction = heading / distance;
         var angle = Vector3.Angle(dog.transform.position, transform.position);
-        Debug.Log($"heading: {heading}");
-        Debug.Log($"distance: {distance}");
-        Debug.Log($"direction: {direction}");
+        //Debug.Log($"heading: {heading}");
+        //Debug.Log($"distance: {distance}");
+        //Debug.Log($"direction: {direction}");
 
         var theta = Mathf.Atan2(heading.x, heading.z) * Mathf.Rad2Deg;
         angle = theta + 180;
 
-        Debug.Log($"angle: {angle}");
+        //Debug.Log($"angle: {angle}");
 
         if (distance < startleDistance)
         {
